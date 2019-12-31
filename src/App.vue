5ed19464438e010756ee5fbbfcac3ca8633ed81f
김대호 <pot@gimdaehoui-MacBook-Pro.local>
2019-12-31 19:35:55 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+        <bodong-header></bodong-header>
+        <bodong-input></bodong-input>
+        <bodong-list></bodong-list>
+        <BodongFooter></BodongFooter>
+    </div>
+</template>
+
+<script>
+    import BodongHeader from './components/BodongHeader'
+    import BodongInput from './components/BodongInput'
+    import BodongList from './components/BodongList'
+    import BodongFooter from './components/BodongFooter'
+
+    export default {
+        components: {
+            'BodongHeader': BodongHeader,
+            'BodongInput': BodongInput,
+            'BodongList': BodongList,
+            'BodongFooter': BodongFooter
+        }
+    }
+</script>
+
+<style>
+    body {
+        text-align: center;
+        background-color: #F6F6F8;
+    }
+
+    input {
+        border-style: groove;
+        width: 200px;
+    }
+
+    button {
+        border-style: groove;
+    }
+
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+    }
+</style>
